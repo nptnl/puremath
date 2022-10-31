e504cf352e96bea78cef42bf4963a11dc7d77b5b
Neptunal <110062414+nptnl@users.noreply.github.com>
2022-10-31 17:55:31 -0400

    fractal update
    its like >10 times faster now i'd say

--- fractal.py
+++ fractal.py
@@ -1,36 +1,40 @@
-from pm import comp
-from render import co2D,image
-
-def julia(c,size=128):
-    # z = z*z + c
-    colist = []
-    for real in range(size):
-        for imag in range(size):
-            point = co2D(real,imag)
-            counter = 0
-            z = comp(4*real/size - 2, 4*real/size - 2)
-            while z.r*z.r + z.i*z.i < 2:
-                z = z*z + c
+# z2 + c
+def julia(cr=0,ci=0,size=64,iterate=64):
+    ppm = open('./plots/current.ppm','w')
+    ppm.write(f'P3\n{2*size} {2*size}\n1\n')
+    for imag in range(-size,size):
+        string = ''
+        for real in range(-size,size):
+            zr,zi = 2*real/size, 2*imag/size
+            counter,small = 0,'0'
+            while zr*zr + zi*zi < 2:
+                zr,zi = zr*zr - zi*zi + cr, 2*zr*zi + ci
                 counter += 1
-                if counter > 100:
-                    colist.append(point)
+                if counter >= iterate:
+                    small = '1'
                     break
-    return image(colist,size)
-def mandelbrot(size=128):
-    colist = []
-    for real in range(size):
-        for imag in range(size):
-            point = co2D(real,imag)
-            counter = 0
-            c = comp(4*real/size - 2, 4*imag/size - 2)
-            z = comp(0,0)
-            while z.r*z.r + z.i*z.i < 2:
-                z = z*z + c
+            string += small
+        for b in string:
+            ppm.write((b+' ')*3)
+        ppm.write('\n')
+def mandelbrot(size=64,iterate=64):
+    ppm = open('./plots/current.ppm','w')
+    ppm.write(f'P3\n{2*size} {2*size}\n1\n')
+    for imag in range(-size,size):
+        string = ''
+        for real in range(-size,size):
+            cr,ci = 2*real/size, 2*imag/size
+            counter,zr,zi,small = 0,0,0,'0'
+            while zr*zr + zi*zi < 2:
+                zr,zi = zr*zr - zi*zi + cr, 2*zr*zi + ci
                 counter += 1
-                if counter > 100:
-                    colist.append(point)
+                if counter >= iterate:
+                    small = '1'
                     break
-    return image(colist,size)
+            string += small
+        for b in string:
+            ppm.write((b+' ')*3)
+        ppm.write('\n')
 
 print('based fractals have arrived')
 based = True
