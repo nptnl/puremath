f5038e3441008e532b9f4bc06256794c1d057a32
Neptunal <110062414+nptnl@users.noreply.github.com>
2022-11-03 13:23:59 -0400

    plot any fractal!
    you can now use any defined function to generate an input-space or parameter-space fractal

--- fractal.py
+++ fractal.py
@@ -1,6 +1,7 @@
 # fatou set and "multibrot" set plotting for
 # f(z) = z2 + c, f(z) = z3 + c
-# working on a general fatou set plotter
+# additional input-space and parameter space plotting for any defined function of z and c
+from pm import comp
 def fatou2(cr=0,ci=0,size=128,iterate=32):
     ppm = open('./plots/current.ppm','w')
     ppm.write(f'P3\n{2*size} {2*size}\n{iterate}\n')
@@ -65,6 +66,43 @@ def bibrot(size=128,iterate=32,sectionr=0,sectioni=0):
             counter,zr,zi = 0,sectionr,sectioni
             while zr*zr + zi*zi < 4:
                 zr,zi = zr*zr*zr - 3*zr*zi*zi + cr, zi*(3*zr*zr - zi*zi) + ci
+                counter += 1
+                if counter >= iterate:
+                    counter = 0
+                    break
+            colorlist.append(counter)
+        for b in colorlist:
+            ppm.write((str(b)+' ')*3)
+        ppm.write('\n')
+
+def ispace(func,c,size=128,iterate=32):
+    ppm = open('./plots/current.ppm','w')
+    ppm.write(f'P3\n{2*size} {2*size}\n{iterate}\n')
+    for imag in range(-size,size):
+        colorlist = []
+        for real in range(-size,size):
+            x = comp(2.0*real/size, 2.0*imag/size)
+            counter = 0
+            while 0.0 < x.r*x.r + x.i*x.i < 4.0:
+                x = func(x,c)
+                counter += 1
+                if counter >= iterate:
+                    counter = 0
+                    break
+            colorlist.append(counter)
+        for b in colorlist:
+            ppm.write((str(b)+' ')*3)
+        ppm.write('\n')
+def pspace(func,section=comp(0,0),size=128,iterate=64):
+    ppm = open('./plots/current.ppm','w')
+    ppm.write(f'P3\n{2*size} {2*size}\n{iterate}\n')
+    for imag in range(-size,size):
+        colorlist = []
+        for real in range(-size,size):
+            c = comp(2.0*real/size, 2.0*imag/size)
+            counter,x = 0,section
+            while x.r*x.r + x.i*x.i < 4.0:
+                x = func(x,c)
                 counter += 1
                 if counter >= iterate:
                     counter = 0
