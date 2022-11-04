23224db1f104f9354eb782ba271a976a3e85ec60
Neptunal <110062414+nptnl@users.noreply.github.com>
2022-11-04 08:53:52 -0400

    formatting and documentation
    needed an update i reckon

--- fractal.py
+++ fractal.py
@@ -16,42 +16,6 @@ def dec2hex(val):
     elif val == 15:
         return 'f'
 
-def fatou2(cr=0,ci=0,size=128,iterate=32):
-    pxl = open('./plots/current.npxl','w')
-    pxl.write(f'{2*size} {2*size}\n1 16\n')
-    for imag in range(-size,size):
-        colorlist = []
-        for real in range(-size,size):
-            zr,zi = 2*real/size, 2*imag/size
-            counter = 0
-            while zr*zr + zi*zi < 4:
-                zr,zi = zr*zr - zi*zi + cr, 2*zr*zi + ci
-                counter += 1
-                if counter >= iterate:
-                    counter = 0
-                    break
-            colorlist.append(counter)
-        for b in colorlist:
-            pxl.write(dec2hex(b * 16 // iterate))
-        pxl.write('\n')
-def mandelbrot(size=128,iterate=32,sectionr=0,sectioni=0):
-    pxl = open('./plots/current.npxl','w')
-    pxl.write(f'{2*size} {2*size}\n1 16\n')
-    for imag in range(-size,size):
-        colorlist = []
-        for real in range(-size,size):
-            cr,ci = 2*real/size, 2*imag/size
-            counter,zr,zi = 0,sectionr,sectioni
-            while zr*zr + zi*zi < 4:
-                zr,zi = zr*zr - zi*zi + cr, 2*zr*zi + ci
-                counter += 1
-                if counter >= iterate:
-                    counter = 0
-                    break
-            colorlist.append(counter)
-        for b in colorlist:
-            pxl.write(dec2hex(b * 16 // iterate))
-        pxl.write('\n')
 def ispace(func,c,size=128,iterate=32):
     pxl = open('./plots/current.npxl','w')
     pxl.write(f'{2*size} {2*size}\n1 16\n')
@@ -80,6 +44,43 @@ def pspace(func,size=128,iterate=64,section=comp(0,0)):
             counter,x = 0,section
             while x.r*x.r + x.i*x.i < 4.0:
                 x = func(x,c)
+                counter += 1
+                if counter >= iterate:
+                    counter = 0
+                    break
+            colorlist.append(counter)
+        for b in colorlist:
+            pxl.write(dec2hex(b * 16 // iterate))
+        pxl.write('\n')
+
+def quadra(cr=0,ci=0,size=128,iterate=32):
+    pxl = open('./plots/current.npxl','w')
+    pxl.write(f'{2*size} {2*size}\n1 16\n')
+    for imag in range(-size,size):
+        colorlist = []
+        for real in range(-size,size):
+            zr,zi = 2*real/size, 2*imag/size
+            counter = 0
+            while zr*zr + zi*zi < 4:
+                zr,zi = zr*zr - zi*zi + cr, 2*zr*zi + ci
+                counter += 1
+                if counter >= iterate:
+                    counter = 0
+                    break
+            colorlist.append(counter)
+        for b in colorlist:
+            pxl.write(dec2hex(b * 16 // iterate))
+        pxl.write('\n')
+def mandelbrot(size=128,iterate=32,sectionr=0,sectioni=0):
+    pxl = open('./plots/current.npxl','w')
+    pxl.write(f'{2*size} {2*size}\n1 16\n')
+    for imag in range(-size,size):
+        colorlist = []
+        for real in range(-size,size):
+            cr,ci = 2*real/size, 2*imag/size
+            counter,zr,zi = 0,sectionr,sectioni
+            while zr*zr + zi*zi < 4:
+                zr,zi = zr*zr - zi*zi + cr, 2*zr*zi + ci
                 counter += 1
                 if counter >= iterate:
                     counter = 0
