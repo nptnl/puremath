44cd4ad452ce2562a72199aa6dc41a90758b7214
Neptunal <110062414+nptnl@users.noreply.github.com>
2023-11-14 15:46:08 -0500

    row reductions!
    now we can basically do anything in linear algebra

--- lin.py
+++ lin.py
@@ -42,6 +42,7 @@ class matrix:
                     entry += self.D[each][row] * other.D[col][each]
                 result.D[col][row] = entry
         return result
+    
     def transpose(self):
         result = makix(self.cols, self.rows)
         for row in range(self.rows):
@@ -54,6 +55,86 @@ class matrix:
             for row in range(self.rows):
                 result.D[col][row] = self.D[col][row] * factor
         return result
+    def cut(self, cut_row, cut_col):
+        result = matrix(self.rows - 1, self.cols - 1)
+        col_add = 0
+        for col in range(self.cols):
+            row_add = 0
+            if col == cut_col:
+                col_add = -1
+                continue
+            for row in range(self.rows):
+                if row == cut_row:
+                    row_add = -1
+                    continue
+                result.D[col + col_add][row + row_add] = self.D[col][row]
+        return result
+    def det(self):
+        if self.rows != self.cols:
+            raise ValueError("matrix must be square to have a determinant")
+        if self.rows == 1:
+            return self.D[0][0]
+        elif self.rows == 2:
+            return self.D[0][0] * self.D[1][1] - self.D[0][1] * self.D[1][0]
+        tot = 0
+        multi = 1
+        for cut_row in range(self.rows):
+            tot += self.D[0][cut_row] * self.cut(cut_row, 0).det() * multi
+            multi = -multi
+        return tot
+    
+    def rowop_swap(self, row1, row2):
+        for col in range(self.cols):
+            self.D[col][row1], self.D[col][row2] = self.D[col][row2], self.D[col][row1]
+    def rowop_add(self, onto_row, from_row, factor):
+        for col in range(self.cols):
+            self.D[col][onto_row] += self.D[col][from_row] * factor
+    def rowop_scl(self, row, factor):
+        for col in range(self.cols):
+            self.D[col][row] *= factor
+    def put_value_in_topleft(self, runcol, runrow):
+        for possible_row in range(runrow, self.rows):
+            if self.D[runcol][possible_row] != 0:
+                self.rowop_swap(runrow, possible_row)
+                return True
+        return False
+    def rid_column_below(self, runcol, runrow):
+        for possible_row in range(runrow+1, self.rows):
+            if self.D[runcol][possible_row] != 0:
+                factor = - self.D[runcol][possible_row] / self.D[runcol][runrow]
+                self.rowop_add(possible_row, runrow, factor)
+                self.D[runcol][possible_row] = 0
+    def rid_column_above(self, runcol, runrow):
+        for possible_row in range(runrow):
+            if self.D[runcol][possible_row] != 0:
+                factor = - self.D[runcol][possible_row] / self.D[runcol][runrow]
+                self.rowop_add(possible_row, runrow, factor)
+                self.D[runcol][possible_row] = 0
+    def echelon(self):
+        runcol = 0
+        for runrow in range(self.rows):
+            pivot_in_this_col = self.put_value_in_topleft(runcol, runrow)
+            if pivot_in_this_col:
+                self.rid_column_below(runcol, runrow)
+                runcol += 1
+            runrow += 1
+    def reduce(self):
+        runcol = 0
+        for runrow in range(self.rows):
+            pivot_in_this_col = self.put_value_in_topleft(runcol, runrow)
+            if pivot_in_this_col:
+                self.rid_column_above(runcol, runrow)
+                self.rid_column_below(runcol, runrow)
+                self.rowop_scl(runrow, 1.0 / self.D[runcol][runrow])
+                runcol += 1
+            runrow += 1
+    def augment(self, extra):
+        if self.rows != len(extra[0]):
+            raise ValueError("augmentation dimensions do not match")
+        self.cols += len(extra)
+        for col in extra:
+            self.D.append(col)
+
 
 class vec(matrix):
     def __init__(self, dim):
